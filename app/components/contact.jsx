ea68d536abf0786fa8824cddae9fd30515cda5d3
Simbolon105 <dewantha.105@gmail.com>
2025-09-04 11:02:20 +0700

    add: contact me component

--- app/components/contact.jsx
+++ app/components/contact.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Image from "next/image";
+import { assets } from "@/assets/assets";
 
 export default function Contact() {
   return (
     <div
       id="contact"
-      className="w-full px-[12%] py-10 scroll-mt-20 bg-[url('/footer-bg-color.png)] bg-no-repeat bg-center bg-[length:90%_auto]"
+      className="w-full px-[12%] py-10 scroll-mt-20 bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-[length:90%_auto]"
     >
       <h4 className="text-center mb-2 text-lg font-Ovo">Connect With Me!</h4>
       <h2 className="text-center text-5xl font-Ovo">Get In Touch</h2>
@@ -13,6 +15,40 @@ export default function Contact() {
         I'd love to hear from you! If you have any questions, comments, or
         feedback, feel free to send me message by using the from below.
       </p>
+
+      <form className="max-w-2xl mx-auto">
+        <div className="grid grid-cols-auto gap-6 mt-10 mb-8">
+          <input
+            type="text"
+            placeholder="Enter Your Name"
+            required
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+          />
+          <input
+            type="email"
+            placeholder="Enter Your Email"
+            required
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+          />
+        </div>
+
+        <textarea
+          rows="6"
+          placeholder="Enter Your Message"
+          required
+          className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6"
+        ></textarea>
+
+        <button
+          type="submit"
+          className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500"
+        >
+          Submit Now{" "}
+          <Image src={assets.right_arrow_white} alt="" className="w-4" />
+        </button>
+
+        <p className="mt-4">Sending...</p>
+      </form>
     </div>
   );
 }
