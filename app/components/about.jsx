9e84b4bdf74f0f13d3ae9825cca3ec0ba7ab8e66
Simbolon105 <dewantha.105@gmail.com>
2025-09-06 10:07:01 +0700

    add: dark mode theme at about, my services and my work section

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { assets, infoList, toolsData } from "@/assets/assets";
 
-export default function About() {
+export default function About({ isDarkMode }) {
   return (
     <div id="about" className="w-full px-[12%] py-10 scroll-mt-20 pt-24">
       <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
@@ -30,16 +30,26 @@ export default function About() {
             {infoList.map(({ icon, iconDark, title, description }, index) => (
               <li
                 key={index}
-                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black"
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-dark-hover/50"
               >
-                <Image src={icon} alt={title} className="w-7 mt-3" />
-                <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
-                <p className="text-gray-600 text-sm">{description}</p>
+                <Image
+                  src={isDarkMode ? iconDark : icon}
+                  alt={title}
+                  className="w-7 mt-3"
+                />
+                <h3 className="my-4 font-semibold text-gray-700 dark:text-white">
+                  {title}
+                </h3>
+                <p className="text-gray-600 text-sm dark:text-white/80">
+                  {description}
+                </p>
               </li>
             ))}
           </ul>
 
-          <h4 className="my-6 text-gray-700 font-Ovo">Tools I Use</h4>
+          <h4 className="my-6 text-gray-700 font-Ovo dark:text-white/80">
+            Tools I Use
+          </h4>
           <ul className="flex items-center gap-3 sm:gap-5">
             {toolsData.map((tool, index) => (
               <li
--- app/components/work.jsx
+++ app/components/work.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { assets, workData } from "@/assets/assets";
 
-export default function Work() {
+export default function Work({ isDarkMode }) {
   return (
     <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
       <h4 className="text-center mb-2 text-lg font-Ovo">My Portfolio</h4>
@@ -15,7 +15,7 @@ export default function Work() {
         expertise in full-stack web development.
       </p>
 
-      <div className="grid grid-cols-auto my-10 gap-5">
+      <div className="grid grid-cols-auto my-10 gap-5 dark:text-black">
         {workData.map((project, index) => (
           <div
             key={index}
@@ -37,11 +37,13 @@ export default function Work() {
 
       <a
         href=""
-        className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500"
+        className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-dark-hover"
       >
         Show More{" "}
         <Image
-          src={assets.right_arrow_bold}
+          src={
+            isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold
+          }
           alt="right arrow"
           className="w-4"
         />
